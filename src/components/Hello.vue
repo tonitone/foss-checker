b8d4387b703ec9b41caa52bfa20c798029c984fa
tonitone <030toni@gmail.com>
2020-10-29 23:22:44 +0100

    refactor: change test

---
+++ src/components/Hello.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="hello">
+    <h1>{{ headline }}</h1>
+    <p>{{ msg }}</p>
+    <p>
+      Check digga
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
+        >vue-cli documentation</a
+      >.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Hello",
+  props: {
+    headline: String,
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+a {
+  color: #ff0000;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Hello headline="FOSS Checker " />
+    <Hello msg="lets read your package.json" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Hello from "@/components/Hello.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    Hello
   }
 };
 </script>
